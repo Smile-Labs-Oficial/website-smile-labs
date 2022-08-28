b7f6d0405eb88a5a4f1d37ccedefb4bd64382e07
smilelabs <smilelabs.oficial@gmail.com>
2022-08-28 13:10:31 -0300

    create component Navbar

---
+++ components/Navbar.jsx
@@ -0,0 +1,41 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import smileLogo from '../public/assets/logo.png';
+
+const Navbar = () => {
+  return (
+    <>
+      <nav className="flex items-center flex-wrap p-3 justify-between">
+        <Link href="/">
+          <a className="inline-flex items-center p-2 mr-4 ">
+            <Image
+              className="rounded-full mt-2.5"
+              src={smileLogo}
+              alt="smile logo"
+              width={50}
+              height={50}
+            />
+          </a>
+        </Link>
+        <div>
+          <Link href="/galery">
+            <a className="inline-flex items-center p-2 mr-4 ">
+              <span className="text-xl text-yellow-400 tracking-wide hover:underline">
+                Galery
+              </span>
+            </a>
+          </Link>
+          <Link href="/contact">
+            <a className="inline-flex items-center p-2 mr-4 ">
+              <span className="text-xl text-yellow-400 tracking-wide hover:underline">
+                Contact
+              </span>
+            </a>
+          </Link>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
