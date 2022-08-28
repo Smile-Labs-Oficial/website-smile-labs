ab62ee51d9eab6484a8861b02acc7bd3e6ca06af
smilelabs <smilelabs.oficial@gmail.com>
2022-08-28 12:13:00 -0300

    create folder components and create prettier archive

---
+++ components/MainContent.jsx
@@ -0,0 +1,30 @@
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
+import smileLogo from '../public/assets/logo.png';
+
+const MainContent = () => {
+  return (
+    <main className={styles.main}>
+      <h1 className={styles.title}>
+        Welcome to{' '}
+        <a
+          className="text-yellow-400"
+          href="https://github.com/Smile-Labs-Oficial"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Smile Labs!
+        </a>
+      </h1>
+      <Image
+        className="rounded-full mt-2.5"
+        src={smileLogo}
+        alt="smile logo"
+        width={250}
+        height={250}
+      />
+    </main>
+  );
+};
+
+export default MainContent;
