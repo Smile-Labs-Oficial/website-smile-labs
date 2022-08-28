ad87c80a46c7721cfb7b552809829bbef815afbd
smilelabs <smilelabs.oficial@gmail.com>
2022-08-28 13:10:52 -0300

    create page contact

---
+++ pages/contact.js
@@ -0,0 +1,13 @@
+import Layout from '../components/Layout';
+
+const contact = () => {
+  return (
+    <>
+      <Layout>
+        <p>Working on that :)</p>
+      </Layout>
+    </>
+  );
+};
+
+export default contact;
