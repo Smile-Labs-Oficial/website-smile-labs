313e7f0c8e6049679f00f6625fbef812c3b184df
smilelabs <smilelabs.oficial@gmail.com>
2022-08-28 13:11:48 -0300

    page galery

---
+++ pages/galery.js
@@ -0,0 +1,13 @@
+import Layout from '../components/Layout';
+
+const galery = () => {
+  return (
+    <>
+      <Layout>
+        <p>Working on that :)</p>
+      </Layout>
+    </>
+  );
+};
+
+export default galery;
