a7d882ea8f2513e9580773b090a2dcf6d95046c3
smilelabs <smilelabs.oficial@gmail.com>
2022-08-28 13:09:02 -0300

    add changes layout component

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,10 +1,12 @@
 import Footer from './Footer';
 import styles from '../styles/Home.module.css';
+import Navbar from './Navbar';
 
 const Layout = ({ children }) => {
   return (
     <div className={styles.container}>
-      {children}
+      <Navbar />
+      <main className={styles.main}>{children}</main>
       <Footer />
     </div>
   );
